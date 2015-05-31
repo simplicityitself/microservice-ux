f52aa8add733b7e6d9ad2ff945aa517f7d996218
David Dawson <david.dawson@simplicityitself.com>
2015-05-31 18:02:55 +0100

    Update redeploy, add a base microux image for broad runtime support

---
+++ runtime/lattice-syslog/redeploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+#docker run quay.io:simplicityitself:deploylattice -e lattice url lattic user lattic password memory disk 
+
+ltc rm lattice_logger
+ltc create lattice_logger quay.io/simplicityitself/lattice-logger -m 200 --no-monitor -e SPRING_PROFILES_ACTIVE=prod
+
