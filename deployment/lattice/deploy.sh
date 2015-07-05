f0b9e292e5d73e930ba70a74c50b1757ad08d92a
David Dawson <david.dawson@simplicityitself.com>
2015-07-05 15:33:07 +0100

    Initial dockerised ltc deployment

---
+++ deployment/lattice/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Target  $LTC_TARGET"
+
+expect -c "
+spawn ./ltc target $LTC_TARGET
+expect -nocase \"Username:\" {send \"$LTC_USER\r\"}
+expect -nocase \"Password:\" {send \"$LTC_PASS\r\";interact}
+"
+
+./ltc target
+
+./ltc rm $LTC_APP
+./ltc create $LTC_APP $LTC_IMAGE -m "${LTC_MEM}" --no-monitor -e "SPRING_PROFILES_ACTIVE=prod"
+
