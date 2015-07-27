c3bebba90a723828ef1db9608897ba48d9d12be6
David Dawson <david.dawson@simplicityitself.com>
2015-07-27 19:23:00 +0100

    Hardcode muon config for now

--- deployment/lattice/deploy.sh
+++ deployment/lattice/deploy.sh
@@ -11,5 +11,5 @@ expect -nocase \"Password:\" {send \"$LTC_PASS\r\";interact}
 ./ltc target
 
 ./ltc rm $LTC_APP
-./ltc create $LTC_APP $LTC_IMAGE -m "${LTC_MEM}" --no-monitor -e "SPRING_PROFILES_ACTIVE=prod"
+./ltc create $LTC_APP $LTC_IMAGE -m "${LTC_MEM}" --no-monitor -e "SPRING_PROFILES_ACTIVE=prod" -e "MUON_CONFIG=/usr/src/app/config.json"
 
