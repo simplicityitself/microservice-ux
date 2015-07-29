db8318651e2d4022d29edab6379c0c7e39702bb4
Nick Wootton <Nicholas.Wootton@bskyb.com>
2015-07-29 16:10:53 +0100

    Amended version

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 apt-get update
+#apt-get upgrade -y
+#apt-get dist-upgrade -y
+#apt-get autoremove -y
 
-apt-get install git -y
-
-apt-get install wget curl build-essential -y
+apt-get install git wget curl build-essential -y
 
 wget -qO- https://get.docker.com/ | sh
 
@@ -13,11 +14,12 @@ apt-get --purge remove nodejs -y
 apt-get install nodejs -y
 apt-get install npm -y
 
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-#git clone https://github.com/simplicityitself/microservicesux.git
-
 apt-get autoremove -y
 
-MUON_ENV = 'test'
-MUON_CONFIG = 'test'
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+git clone https://github.com/simplicityitself/microservice-ux.git
+
+sudo npm install -g muon-core
+
+#muon setup
