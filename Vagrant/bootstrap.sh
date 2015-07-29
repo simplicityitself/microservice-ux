52002aeb35478b1f5d7f255cc53abfbfc31cfb87
Gawain Hammond <spam.gawain@gmail.com>
2015-07-29 22:36:41 +0100

    vagrant java update for base install

--- Vagrant/bootstrap.sh
+++ Vagrant/bootstrap.sh
@@ -18,8 +18,26 @@ apt-get autoremove -y
 
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 
+wget https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein
+chmod +x lein
+mv lein /usr/bin/
+
+mkdir muon
+cd muon
 git clone https://github.com/simplicityitself/microservice-ux.git
+git clone http://github.com/photonevents/photon.git
 
 sudo npm install -g muon-core
+
+
+
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update -y
+sudo apt-get install oracle-java8-installer -y
+
+
+sudo chown -R vagrant:vagrant /home/vagrant
 
 #muon setup
