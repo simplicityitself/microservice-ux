5d5be154386a2134fdfad4661d55769150d52d19
Nick Wootton <Nicholas.Wootton@bskyb.com>
2015-07-30 12:43:39 +0100

    Added environment declaration and notes

--- runtime/rabbitmq/runrabbit.sh
+++ runtime/rabbitmq/runrabbit.sh
@@ -1,1 +1,5 @@
+#!/usr/bin/env bash
+
+#Uses RabbitMQ server with management plugin based on version 3.5.4. RabbitMQ images do not support latest with management console
+
 docker run -d --hostname muon-rabbit --name rabbit-muon -e RABBITMQ_DEFAULT_USER=muon -e RABBITMQ_DEFAULT_PASS=microservices -p 15672:15672 -p 5672:5672 rabbitmq:3.5.4-management
