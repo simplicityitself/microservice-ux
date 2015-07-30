f7d4f9709474cbf1124d9733ed20a6006c7571ad
Nick Wootton <Nicholas.Wootton@bskyb.com>
2015-07-30 12:52:00 +0100

    Forgot the sudo in the RabbitMQ start!

--- runtime/rabbitmq/runrabbit.sh
+++ runtime/rabbitmq/runrabbit.sh
@@ -2,4 +2,4 @@
 
 #Uses RabbitMQ server with management plugin based on version 3.5.4. RabbitMQ images do not support latest with management console
 
-docker run -d --hostname muon-rabbit --name rabbit-muon -e RABBITMQ_DEFAULT_USER=muon -e RABBITMQ_DEFAULT_PASS=microservices -p 15672:15672 -p 5672:5672 rabbitmq:3.5.4-management
+sudo docker run -d --hostname muon-rabbit --name rabbit-muon -e RABBITMQ_DEFAULT_USER=muon -e RABBITMQ_DEFAULT_PASS=microservices -p 15672:15672 -p 5672:5672 rabbitmq:3.5.4-management
