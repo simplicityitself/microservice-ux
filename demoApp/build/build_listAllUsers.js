2ef5e4e27727e485b9c7db3ab6980623f6c91104
Nick Wootton <Nicholas.Wootton@bskyb.com>
2015-08-10 11:42:27 +0100

    Tidying and removing used items to try and fix the 404 return error in NodeJS app

--- demoApp/build/build_listAllUsers.js
+++ demoApp/build/build_listAllUsers.js
@@ -77,7 +77,7 @@ buildProjection = function(){
   var projName = "UserList";
 
   //actual projection reduction function
-  var projString = "function eventHandler(state, event) {  var user = event.payload.user;  if (!(user.last in state)) {state[user.last] = {};  }  state[user.last].id = user.id;  state[user.last].fullname = user.first + \' \' + user.last;  if(user.last.length > 8) {state[user.last].username = (user.last.substring + user.first.charAt(0)).toLowerCase();  }  state[user.last].username = (user.last + user.first.charAt(0)).toLowerCase();  state[user.last].password = user.password;  state[user.last].logins = [];  return state;}";
+  var projString = "function eventHandler(state, event) {  var user = event.payload.user;  if (!(user.last in state)) {state[user.last] = {};  }  state[user.last].id = user.id;  state[user.last].fullname = user.first + \' \' + user.last;  if(user.last.length > 8) {state[user.last].username = (user.last.substring + user.first.charAt(0)).toLowerCase();  }  state[user.last].username = (user.last + user.first.charAt(0)).toLowerCase();  state[user.last].first = user.first;  state[user.last].last = user.last;  state[user.last].password = user.password;  return state;}";
 
   //Define projection
   var projConf = {"projection-name" : projName,
